4769a076b40e3fe7188c071c0cb6966c408bfc67
Kouvousis <dexous1996@gmail.com>
2024-06-04 20:55:41 +0300

    updated ch12, added ch14 and ch15

---
+++ src/gr/aueb/cf/ch14/CodingFactoryMain.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch14;
+
+public class CodingFactoryMain {
+
+    public static void main(String[] args) {
+        CodingFactory codingFactory = CodingFactory.getInstance();
+
+    }
+}
---
+++ src/gr/aueb/cf/ch15/shape/Shape.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch15.shape;
+
+/**
+ * POJO (PLain Old Java Object)
+ */
+public class Shape {
+
+    public void draw() {
+        System.out.println("Drawing a shape");
+    }
+}
